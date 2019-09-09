9277895bdd44b33a9db8e54736f091bc14bd241e
Zachary Nelson <zbnelson@users.campus>
2019-09-09 17:14:37 -0500

    added nugs and wurst

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        int nuggetCount = 6;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }
+
+        public void AddNugget()
+        {
+            this.Ingredients.Add("Chicken Nugget");
+            this.Price = Price + .25;
+            this.Calories = Calories + 59;
+        }
+
+    }
+}
