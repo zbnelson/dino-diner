bbd5532d96495e707ba856bb5a6a1fbd5c35128e
Zachary Nelson <zbnelson@users.campus>
2019-10-09 17:14:29 -0500

    added git files

---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuCategorySelection.xaml
+    /// </summary>
+    public partial class MenuCategorySelection : Page
+    {
+        public MenuCategorySelection()
+        {
+            InitializeComponent();
+        }
+
+        void SelectCombo(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new ComboSelection());
+        }
+
+        void SelectEntree(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new EntreeSelection());
+        }
+
+        void SelectSide(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new SideSelection());
+        }
+
+        void SelectDrink(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new DrinkSelection());
+        }
+    }
+}
