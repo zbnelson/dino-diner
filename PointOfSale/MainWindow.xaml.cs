0ddec84571f27c9ebdd023b2125e3e8e0f0e7a78
Zachary Nelson <zbnelson@users.campus>
2019-10-21 17:01:26 -0500

    Added orderlist.xaml

--- PointOfSale/MainWindow.xaml.cs
+++ PointOfSale/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DinoDiner.Menu;
 
 namespace PointOfSale
 {
@@ -29,6 +30,15 @@ namespace PointOfSale
         public MainWindow()
         {
             InitializeComponent();
+            Order order = new Order();
+            order.Items.Add(new PrehistoricPBJ());
+            order.Items.Add(new Sodasaurus());
+            order.Items.Add(new Fryceritops());
+            SteakosaurusBurger sb = new SteakosaurusBurger();
+            sb.HoldPickle();
+            sb.HoldMustard();
+            order.Items.Add(sb);
+            DataContext = order;
         }
     }
 }
